c3cbb604282c4ecbb5a9f8486a8d7a25b73cd207
Edgar Sun <esun@voteb.com>
2023-02-04 21:41:23 +0900

    use native proto builder to replace betterproto

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unity-grpc-build-proto-pipe',
-    version='0.0.8',
+    version='0.0.9',
     license='MIT',
     description='protobuf for grpc Pipe',
     author='esun',
