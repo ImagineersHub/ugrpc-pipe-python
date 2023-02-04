6284d97da7467b928af07489990239f179941e96
Edgar Sun <esun@voteb.com>
2023-02-04 21:45:31 +0900

    removed betterproto dep

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@ setup(
     install_requires=[
         'grpcio==1.50.0',
         'grpcio-tools==1.50.0',
-        'protobuf==4.21.8',
-        'betterproto[compiler]>=2.0.*'],
+        'protobuf==4.21.8'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3.10'
