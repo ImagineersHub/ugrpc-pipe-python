7940a7e3c29697799c968713172169e5158ba358
esun <esun@voteb.com>
2024-01-08 18:11:59 +0900

    updated package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ugrpc_pipe',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     description='protobuf for grpc Pipe',
     author='esun',
